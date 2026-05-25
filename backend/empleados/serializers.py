124307fbee1fe16c6b5e947a2848a354605a8633
AnthonyAndino <anthonyandino959@gmail.com>
2026-05-24 19:55:35 -0600

    Agregados serializers de Asistencia y Nomina

--- backend/empleados/serializers.py
+++ backend/empleados/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
-from .models import Empleado
+from .models import Empleado, Asistencia, Nomina
 
 class EmpleadoSerializer(serializers.ModelSerializer):
     idEmpleado = serializers.IntegerField(read_only=True)
-
+    
     class Meta:
         model = Empleado
         fields = ['idEmpleado', 'nombre', 'departamento', 'sueldo']
-
+        
     def validate_nombre(self, value: str):
         v = (value or '').strip()
         if not v:
@@ -26,3 +26,21 @@ class EmpleadoSerializer(serializers.ModelSerializer):
         if value <= 0:
             raise serializers.ValidationError('El sueldo debe ser mayor a 0')
         return value
+    
+class AsistenciaSerializer(serializers.ModelSerializer):
+    idAsistencia = serializers.IntegerField(read_only=True)
+    nombre_empleado = serializers.CharField(source='empleado.nombre', read_only=True)
+    
+    class Meta:
+        model = Asistencia
+        fields = ['idAsistencia', 'empleado', 'nombre_empleado', 'fecha', 'hora_entrada', 'hora_salida', 'estado']
+
+class NominaSerializer(serializers.ModelSerializer):
+    idNomina = serializers.IntegerField(read_only=True)
+    nombre_empleado = serializers.CharField(source='empleado.nombre', read_only=True)
+    
+    class Meta:
+        model = Nomina
+        fields = ['idNomina', 'empleado', 'nombre_empleado', 'fecha_pago', 'sueldo_base', 'deducciones', 'bonos', 'sueldo_neto']
+        
+        
