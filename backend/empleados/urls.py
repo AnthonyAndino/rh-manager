cdbea2057410b9e4e879e972331d8256bbd75f63
AnthonyAndino <anthonyandino959@gmail.com>
2026-05-24 20:02:45 -0600

    Agregadas rutas de Asistencia y Nomina

--- backend/empleados/urls.py
+++ backend/empleados/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import (
     EmpleadoListCreateAPIView,
-    EmpleadoRetrieveUpdateDestroyAPIView
+    EmpleadoRetrieveUpdateDestroyAPIView,
+    AsistenciaListCreateAPIView,
+    NominaListCreateAPIView
 )
 
 urlpatterns = [
     path('api/empleados', EmpleadoListCreateAPIView.as_view(), name='empleado-list-create'),
     path('api/empleados/<int:pk>', EmpleadoRetrieveUpdateDestroyAPIView.as_view(), name='empleado-detail'),
+    path('api/asistencias', AsistenciaListCreateAPIView.as_view(), name='asistencia-list-create'),
+    path('api/nominas', NominaListCreateAPIView.as_view(), name='nomina-list'),
 ]
